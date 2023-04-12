be958f5ddbe161f9dfb6496fc271193434d3071d
Henrique <hpeluzio@gmail.com>
2023-04-12 16:52:23 -0300

    Structure and augumentation done

--- structure-cats-and-dogs.py
+++ structure-cats-and-dogs.py
@@ -20,3 +20,26 @@ classifier.add(Dense(units = 1, activation = 'sigmoid'))
 classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy',
                metrics = ['accuracy'])
 
+generator_training = ImageDataGenerator(rescale = 1./255,
+                                        rotation_range = 7,
+                                        horizontal_flip = True,
+                                        shear_range = 0.2,
+                                        height_shift_range = 0.07,
+                                        zoom_range = 0.2)
+
+generator_test= ImageDataGenerator(rescale = 1./255)
+
+base_training = generator_training.flow_from_directory('dataset/training_set', 
+                                                       target_size = (64, 64),
+                                                       batch_size = 32,
+                                                       class_mode = 'binary')
+
+base_test = generator_test.flow_from_directory('dataset/test_set', 
+                                                       target_size = (64, 64),
+                                                       batch_size = 32,
+                                                       class_mode = 'binary')
+
+classifier.fit_generator(base_training, steps_per_epoch = 4000 / 32,
+                         epochs = 5, validation_data = base_test,
+                         validation_steps = 1000 / 128)
+
